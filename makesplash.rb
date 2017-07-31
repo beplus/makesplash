7dc537729b4b3f0a9ccf1324590cd412e40bf457
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-31 14:19:38 +0200

    makesplash version v0.0.4

--- makesplash.rb
+++ makesplash.rb
@@ -1,9 +1,9 @@
 class Makesplash < Formula
   desc "Generate mobile app icons in all resolutions for both iOS and Android"
   homepage "https://github.com/beplus/makesplash"
-  url "https://github.com/beplus/makesplash/releases/download/v0.0.1/makesplash_0.0.1_darwin_amd64.tar.gz"
-  version "0.0.1"
-  sha256 "883b80944a9217ab2b239997508a6d24515ce3951e5d160d5bcf25cc201cb97b"
+  url "https://github.com/beplus/makesplash/releases/download/v0.0.4/makesplash_0.0.4_darwin_amd64.tar.gz"
+  version "0.0.4"
+  sha256 "95380c2b848ac3c30983d977f4566f17da16fe55d9ae7ec700c59d3753b665c6"
 
   def install
     bin.install "makesplash"
